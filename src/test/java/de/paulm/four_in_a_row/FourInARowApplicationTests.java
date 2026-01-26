bb3391d6e119d3bcefc1da8ec3bd8089ca718e95
SebPortfolio <Sebastian.Koch@Student.HTW-Berlin.de>
2026-01-26 11:30:36 +0100

    spring initializr

---
+++ src/test/java/de/paulm/four_in_a_row/FourInARowApplicationTests.java
@@ -0,0 +1,13 @@
+package de.paulm.four_in_a_row;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FourInARowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
