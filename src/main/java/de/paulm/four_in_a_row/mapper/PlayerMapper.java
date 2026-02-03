3875289d182f6439e665574afce7e9310673ea72
KochSebastianHTW <Sebastian.Koch@Student.HTW-Berlin.de>
2026-02-03 15:04:06 +0100

    Refactor PlayerMapper to enhance mapping logic for PlayerDto and PlayerProfile

--- src/main/java/de/paulm/four_in_a_row/mapper/PlayerMapper.java
+++ src/main/java/de/paulm/four_in_a_row/mapper/PlayerMapper.java
@@ -16,5 +16,11 @@ public interface PlayerMapper {
     @Mapping(source = "statistic.lastPlayedOn", target = "lastPlayedOn")
     PlayerDto toDto(PlayerProfile playerProfile);
 
+    @Mapping(ignore = true, target = "email")
+    @Mapping(source = "totalGames", target = "statistic.totalGames")
+    @Mapping(source = "gamesWon", target = "statistic.gamesWon")
+    @Mapping(source = "gamesLost", target = "statistic.gamesLost")
+    @Mapping(source = "gamesSurrendered", target = "statistic.gamesSurrendered")
+    @Mapping(source = "lastPlayedOn", target = "statistic.lastPlayedOn")
     PlayerProfile toProfileEntity(PlayerDto playerProfileDto);
 }
