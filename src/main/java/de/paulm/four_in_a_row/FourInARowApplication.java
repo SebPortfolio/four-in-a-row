c5d87e79091af7ead7c31c48e93af3baeaa1158f
SebPortfolio <Sebastian.Koch@Student.HTW-Berlin.de>
2026-02-27 13:55:48 +0100

    feat(api): expand user management and administration definitions - Massive update on OpenAPi yaml with new seperated endpoints for user management and banning for all and administration - Configure OpenAPI generator in pom.xml to map DateTime to java.time.LocalDateTime - Clean up component scanning in FourInARowApplication

--- src/main/java/de/paulm/four_in_a_row/FourInARowApplication.java
+++ src/main/java/de/paulm/four_in_a_row/FourInARowApplication.java
@@ -7,7 +7,6 @@ import org.springframework.context.annotation.ComponentScan;
 @SpringBootApplication
 @ComponentScan(basePackages = {
         "de.paulm.four_in_a_row", // Mein Code
-        "de.paulm.api" // Die generierten Controller/Interfaces
 })
 public class FourInARowApplication {
 
