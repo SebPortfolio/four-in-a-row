bf7ac9f57a90caadfa2cb31266bc65f0b8d6509a
SebPortfolio <Sebastian.Koch@Student.HTW-Berlin.de>
2026-03-18 14:30:22 +0100

    feat(security): update CORS origins for docker environment - add localhost:25603 to allowed origins

--- src/main/java/de/paulm/four_in_a_row/web/security/SecurityConfig.java
+++ src/main/java/de/paulm/four_in_a_row/web/security/SecurityConfig.java
@@ -103,7 +103,9 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:25610")); // TODO: für Prod korrekte origin
+        config.setAllowedOrigins(List.of(
+                "http://localhost:25603",
+                "http://localhost:25610"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("Authorization", "Cache-Control", "Content-Type", "X-XSRF-TOKEN"));
         config.setAllowCredentials(true);
